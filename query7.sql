1b81a1cf0cf34c43b5deb3f0b3f115f52914450c
Renee Jiang <reneejiang@Renees-MacBook-Pro.local>
2023-02-19 22:53:19 -0600

    ENDING IT ALL (query7 added)

--- query7.sql
+++ query7.sql
@@ -1,4 +1,7 @@
-Select *
-From bid b
-Where b.amount >100.0
-
+select count(*) from (
+select count (ItemCategory.category_ID)
+from Bid, itemcategory
+where bid.Item_ID = ItemCategory.Item_ID
+and bid.Amount > 100
+group by category_ID
+)
