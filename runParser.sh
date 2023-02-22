a7e92a3eac5024b23f8887f090d4c0d01b36f094
Renee Jiang <reneejiang@Renees-MacBook-Pro.local>
2023-02-22 17:14:01 -0600

    Parser modified to remove all dat files

--- runParser.sh
+++ runParser.sh
@@ -1,19 +1,27 @@
 JSON_DIR="./ebay_data"
 SCRIPT_PATH="./EbayParser.py"
-CATEGORY_TB="./ItemCategory.dat"
+CATEGORY_TB="./Category.dat"
+ITEMCATEGORY_TB="./ItemCategory.dat"
 ITEMS_TB="./Item.dat"
 BIDS_TB="./Bid.dat"
 USERS_TB="./User.dat"
+
+
 ARGS=””
 rm -f $CATEGORY_TB
+rm -f $ITEMCATEGORY_TB
 rm -f $ITEMS_TB
 rm -f $BIDS_TB
 rm -f $USERS_TB
-
+rm -f itemCategories.txt
+rm -f categories.txt
+ 
   python $SCRIPT_PATH $JSON_DIR/items-*.json
+
 
   python -c 'from EbayParser import create_categories_tables; create_categories_tables()'
 
+
 sort -u $USERS_TB -o $USERS_TB
-sort -u $CATEGORY_TB -o $CATEGORY_TB
+sort -u $ITEMCATEGORY_TB -o $ITEMCATEGORY_TB
 exit
